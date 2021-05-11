a4b06d2328850c891cd6bbb0fe4423e07245edb6
yren.go <yren@gm.slc.edu>
2021-05-10 23:59:51 -0400

    AviationalMap public class adopting MKMapView

---
+++ Levitation/Levitation/AviationalMap.swift
@@ -0,0 +1,16 @@
+//
+//  AviationalMap.swift
+//  Levitation
+//
+//  Created by Jerry Ren on ~/~/21.
+//
+
+import MapKit
+
+public class AviationalMap: MKMapView {
+        
+    public required init?(coder: NSCoder) {
+        return nil
+    }
+    
+}
