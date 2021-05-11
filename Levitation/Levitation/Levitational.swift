2cfe3e823053e46b30a13143f6248bc6bfd63230
jerry.explodes <jerryexplodes@gmail.com>
2021-05-11 00:12:33 -0400

    Protocol Levitating carrying property of type CLLocationCoordinate2D

---
+++ Levitation/Levitation/Levitational.swift
@@ -0,0 +1,12 @@
+//
+//  Levitational.swift
+//  Levitation
+//
+//  Created by Jerry Ren on ~/~/21.
+//
+
+import MapKit
+
+public protocol Levitating {
+    var coordinate: CLLocationCoordinate2D { get }
+}
