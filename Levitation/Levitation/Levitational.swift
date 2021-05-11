7b71f78583a36dc17aaf5ca28a5dd4f39812c51c
jerry.explodes <jerryexplodes@gmail.com>
2021-05-11 00:25:30 -0400

    Extending MKMapCamera & acquiring LevitatingProtocol

--- Levitation/Levitation/Levitational.swift
+++ Levitation/Levitation/Levitational.swift
@@ -7,6 +7,12 @@
 
 import MapKit
 
+extension MKMapCamera: Levitating {
+    public var coordinate2D: CLLocationCoordinate2D {
+        return self.centerCoordinate
+    }
+}
+
 public protocol Levitating {
-    var coordinate: CLLocationCoordinate2D { get }
+    var coordinate2D: CLLocationCoordinate2D { get }
 }
