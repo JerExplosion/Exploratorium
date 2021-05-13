b8163a5f8c539b974ac94945471a6b07b8fc4780
jerry.explodes <jerryren.ios@gmail.com>
2021-05-12 23:59:22 -0400

    LevitationalShashinki .init .init

---
+++ Levitation/Levitation/LevitationalShashinki.swift
@@ -0,0 +1,18 @@
+//
+//  LevitationalShashinki.swift
+//  Levitation
+//
+//  Created by Jerry Ren on ~/~/21.
+//
+
+import MapKit
+
+public class LevitationalShashinki {
+    
+    private lazy var mappingShashinki: MKMapCamera = {
+        let ssk = MKMapCamera()
+        return ssk
+    }()
+    
+}
+
