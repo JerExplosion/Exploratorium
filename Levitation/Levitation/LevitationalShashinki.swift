4b83210985a6fb31b2c14d150b5674ffe5c3faf2
jerry.explodes <jerryren.ios@gmail.com>
2021-05-13 21:34:59 -0400

    Building inner struct FundamentalConfigurations with initializers

--- Levitation/Levitation/LevitationalShashinki.swift
+++ Levitation/Levitation/LevitationalShashinki.swift
@@ -9,6 +9,18 @@ import MapKit
 
 public class LevitationalShashinki {
     
+    public struct FundamentalConfigurations {
+        public var viewingAngle: Float
+        public var altitude: CLLocationDistance
+        public var duration: TimeInterval
+        
+        public init(viewingAngle: Float, altitude: CLLocationDistance, duration: TimeInterval) {
+            self.viewingAngle = viewingAngle
+            self.altitude = altitude
+            self.duration = duration
+        }
+    }
+    
     private lazy var mappingShashinki: MKMapCamera = {
         let ssk = MKMapCamera()
         return ssk
