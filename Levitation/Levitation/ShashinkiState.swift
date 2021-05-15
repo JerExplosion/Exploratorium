1daf80d6626b17779af8d8b51c3786466e9daf8e
jer .explosion <jerryren.ios@gmail.com>
2021-05-14 23:58:42 -0400

    ShashinkiState enum coming into the picture

---
+++ Levitation/Levitation/ShashinkiState.swift
@@ -0,0 +1,16 @@
+//
+//  ShashinkiState.swift
+//  Levitation
+//
+//  Created by Jerry Ren on 5/14/21.
+//
+
+import Foundation
+
+public extension LevitationalShashinki {
+    enum ShashinkiState: String, CaseIterable, Hashable, Equatable {
+        case hasStopped
+        case hasStarted
+    }
+}
+
