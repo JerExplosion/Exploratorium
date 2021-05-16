809e802b2260d3b28aa8651dfd226b41e2a21d73
jerry .explodes <jerryexplodes@gmail.com>
2021-05-16 16:57:12 -0400

    Adding headingDirection into FundamentalConfiguration's initializers

--- Levitation/Levitation/Shashinki+Configurations.swift
+++ Levitation/Levitation/Shashinki+Configurations.swift
@@ -10,11 +10,13 @@ import CoreLocation
 extension LevitationalShashinki {
     public struct FundamentalConfigurations {
         public var viewingAngle: Float
+        public var headingDirection: CLLocationDirection
         public var altitude: CLLocationDistance
         public var duration: TimeInterval
         
-        public init(viewingAngle: Float, altitude: CLLocationDistance, duration: TimeInterval) {
+        public init(viewingAngle: Float, headingDirection: CLLocationDirection, altitude: CLLocationDistance, duration: TimeInterval) {
             self.viewingAngle = viewingAngle
+            self.headingDirection = headingDirection
             self.altitude = altitude
             self.duration = duration
         }
