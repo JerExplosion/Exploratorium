a0e346374355b27fadf724c9ee4d10c68f452fbb
jerry .explodes <jerryexplodes@gmail.com>
2021-05-16 18:14:34 -0400

    Failable initializer (rawValue) - satellite/hybrid/standard

--- Levitation/Levitation/Aviational+MapType.swift
+++ Levitation/Levitation/Aviational+MapType.swift
@@ -28,4 +28,17 @@ extension AviationalMap.MapType: RawRepresentable {
             return .standard
         }
     }
+    
+    public init?(rawValue: RawValue) {
+        switch rawValue {
+        case .satelliteFlyover:
+            self = .satelliteLevitation
+        case .hybridFlyover:
+            self = .hybridLevitation
+        case .standard:
+            self = .standard
+        default:
+            return nil
+        }
+    }
 }
