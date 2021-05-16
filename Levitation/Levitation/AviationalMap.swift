78f04f3d6aa7ae20d92ea77989ba4ea8d3a36dcb
jerry .explodes <jerryexplodes@gmail.com>
2021-05-16 18:20:40 -0400

    Setting computed property aviationalMapType

--- Levitation/Levitation/AviationalMap.swift
+++ Levitation/Levitation/AviationalMap.swift
@@ -9,9 +9,18 @@ import MapKit
 
 public class AviationalMap: MKMapView {
     
-    open lazy var shashinki: LevitationalShashinki = {
+    open lazy var aviationalShashinki: LevitationalShashinki = {
         return LevitationalShashinki(mkmapView: self, config: .defaultConfig)
     }()
+    
+    public var aviationalMapType: MapType? {
+        set {
+            newValue.flatMap { self.mapType = $0.rawValue }
+        }
+        get {
+            return MapType(rawValue: self.mapType)
+        }
+    }
         
     public required init?(coder: NSCoder) {
         return nil
