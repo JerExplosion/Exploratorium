4c0fa4043a1687734e1b812c65992e8ceeb615b6
jer .explosion <jerryren.ios@gmail.com>
2021-05-18 22:01:13 -0400

    AviationalChizu - loadview / required init / deinit

--- Levitation/Levitation/AviationalChizuViewController.swift
+++ Levitation/Levitation/AviationalChizuViewController.swift
@@ -9,4 +9,19 @@ import UIKit
 
 open class AviationalChizuViewController: UIViewController {
     open var aviationalMapView: AviationalMap?
+    
+    open var aviation: Levitating? {
+        didSet {
+            
+        }
+    }
+    public required init?(coder: NSCoder) {
+        return nil
+    }
+    deinit {
+        
+    }
+    open override func loadView() {
+        self.view = self.aviationalMapView
+    }
 }
