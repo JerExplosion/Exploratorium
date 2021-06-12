5ebad0dcb69a246410110c5e3603b26bee1d5dea
jer .explosion <jerryren.ios@gmail.com>
2021-06-11 22:47:02 -0400

    Composing private function flightExperiment to play around with

--- Levitation/Levitation/LevitationalViewController.swift
+++ Levitation/Levitation/LevitationalViewController.swift
@@ -13,7 +13,17 @@ final class LevitationalViewController: UIViewController, CLLocationManagerDeleg
     override func viewDidLoad() {
         super.viewDidLoad()
         locationDealer.delegate = self
+        flightExperiment()
     }
+    
+    private func flightExperiment() {
+        let renzu = FlyoverCamera(mapView: slyMap, configuration: FlyoverCamera.Configuration(duration: 5, altitude: 1261, pitch: 65.0, headingStep: 75))
+        renzu.start(flyover: FlyoverAwesomePlace.newYork)
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(222)) {
+            renzu.stop()
+        }
+    }
+    
     @IBOutlet weak var slyMap: MKMapView! {
         didSet {
             slyMap.isUserInteractionEnabled = true
